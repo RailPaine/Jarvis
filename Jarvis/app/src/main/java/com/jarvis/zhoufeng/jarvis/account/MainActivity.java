ba70646f1155ebd73f05682c2fd7f3e551b3333d
zhoufeng <18883340065@163.com>
2017-07-14 15:40:04 +0800

    Developing

---
+++ Jarvis/app/src/main/java/com/jarvis/zhoufeng/jarvis/account/MainActivity.java
@@ -0,0 +1,16 @@
+package com.jarvis.zhoufeng.jarvis.account;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+/**
+ * Created by zhoufeng on 2017/7/14.
+ */
+
+public class MainActivity extends Activity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
